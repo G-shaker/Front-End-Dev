7cded1d71ebfdcbd61d750c7fde624b8ee34ebdb
Ghofran Shaker <ghofran.shaker@kaust.edu.sa>
2019-11-12 16:17:28 +0300

    init

---
+++ Weather Journal App/server.js
@@ -0,0 +1,45 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+// Middleware
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+
+function listening(){
+    console.log('server running');
+    console.log(`running on localhost: ${port}`);
+};
+
+// GET route that returns projectData
+app.get('/all', (request, response)=> {
+  response.send(projectData);
+});
+
+// POST route
+app.post('/add', (req, res)=> {
+  console.log(req.body)
+  const d = req.body;
+  projectData['temp'] = d.temp;
+  projectData['date'] = d.date;
+  projectData['resp'] = d.resp;
+
+  console.log('projectData: ', projectData);
+});
